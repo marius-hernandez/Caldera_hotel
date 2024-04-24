8361e03615399c2dac5b3b8b8d3f2f9199573f63
marius-hernandez <118230398+marius-hernandez@users.noreply.github.com>
2024-04-24 08:21:40 +0800

    updating the button component

--- src/components/Button.java
+++ src/components/Button.java
@@ -5,10 +5,10 @@ import javax.swing.JButton;
 import java.awt.Color;
 import java.awt.Font;
 
+// this is for the properties of custom JButton
 public class Button extends JButton {
 	private static final long serialVersionUID =11L; //serialization
     public Button(String text) {
-    	
         super(text);
         setFont(new Font("Arial", Font.PLAIN, 12));
         setFont(new Font("Trebuchet MS", Font.PLAIN, 16));
